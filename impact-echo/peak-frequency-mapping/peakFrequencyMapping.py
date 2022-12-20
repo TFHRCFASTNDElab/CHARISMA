8aca4a491ec1af8d1b2a5a532a8579901771a315
FHWA's Advanced Sensing Technology & Nondestructive Evaluation Laboratory <rahul.torlapati.ctr@dot.gov>
2022-12-20 12:39:29 -0500

    Bug fixes

--- impact-echo/peak-frequency-mapping/peakFrequencyMapping.py
+++ impact-echo/peak-frequency-mapping/peakFrequencyMapping.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env/python3
 # -*- coding: utf-8 -*-
 import os
-from glob import glob
 from scipy import signal
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import sys
+from plotly import io
+import plotly.express as px
 from natsort import natsorted
 
-
-def peakFrequencyMapping(filelist, outputfilename, f_s, flag):
+colorscale = [
+    [0, 'rgb(133,0,0)'], #2
+    [0.076, 'rgb(202,0,0)'], #3
+    [0.153, 'rgb(255,16,0)'], #4
+    [0.230, 'rgb(255,96,0)'], #5
+    [0.307, 'rgb(255,175,0)'], #6
+    [0.384, 'rgb(238,255,17)'], #7
+    [0.461, 'rgb(175,255,80)'], #8
+    [0.538, 'rgb(91,255,163)'], #9
+    [0.615, 'rgb(16,255,239)'], #10
+    [0.692, 'rgb(0,175,255)'], #11
+    [0.769, 'rgb(0,111,255)'], #12
+    [0.846, 'rgb(0,32,255)'], #13
+    [0.923, 'rgb(0,0,216)'], #14
+    [1, 'rgb(0,0,158)'] #15
+]
+def peakFrequencyMapping(filelist, f_s, flag):
     
     for entry in filelist:
-        f = open(path+'/'+entry, "r")
+        f = open(path_+'/'+entry, "r")
         data = f.read()
         data = data.split('\n')
         if data[-1] == "":
@@ -37,19 +51,12 @@ def peakFrequencyMapping(filelist, outputfilename, f_s, flag):
     index = natsorted(list(set(YLocation)))
     cols = natsorted(list(set(XLocation)))
     fdf = pd.DataFrame(np.reshape(FrequencyVal, (9,28)), index = index, columns = cols)
-    cmap = sns.cm.rocket_r
-    f, ax = plt.subplots(figsize=(12,4))
-    plt.axis('on')
     if flag == 1:
-        ax = sns.heatmap(fdf, cbar = True, annot=True, cmap=cmap, cbar_kws={'label': 'kHz'})
-    else:
-        ax = sns.heatmap(fdf, cbar = True, annot=False, cmap=cmap, cbar_kws={'label': 'kHz'})
-    cbar = ax.collections[0].colorbar
-    cbar.ax.tick_params(labelsize=10)
-    ax.figure.axes[-1].yaxis.label.set_size(10)
-    ax.set_xticklabels(cols, fontsize=10)
-    ax.set_yticklabels(index, fontsize=10)
-    f.savefig(outputfilename+'.png')
+        fig = px.imshow(fdf, text_auto = False, color_continuous_scale = colorscale, zmin=2, zmax=15)
+    elif flag == 2:
+        fig = px.imshow(fdf, text_auto = True, color_continuous_scale = colorscale, zmin=2, zmax=15)
+    fig.update_layout(coloraxis_colorbar=dict(len=0.5, thickness=15))
+    io.write_image(fig, 'output.png')
     
     
 if __name__ == "__main__":
@@ -57,14 +64,10 @@ if __name__ == "__main__":
     for a in sys.argv:
         if a == '-i':
             path_ = sys.argv[2]
-        if a == '-o':
-            outputfilename_ = str(sys.argv[4])
         if a == '-f':
-            f_s_ = sys.argv[6]
+            f_s_ = int(sys.argv[4])
         if a == '-a':
-            annotation_ = sys.argv[8]
-        elif:
-            print("Flag error")
+            annotation_ = int(sys.argv[6])
     
     filelist_ = []
     files  = os.listdir(path_)
@@ -75,7 +78,7 @@ if __name__ == "__main__":
     YLocation = []
     XLocation = []
     FrequencyVal = []
-    
-    peakFrequencyMapping(filelist_, outputfilename_, f_s_, annotation_)
+        
+    peakFrequencyMapping(filelist_, f_s_, annotation_)
 
 
