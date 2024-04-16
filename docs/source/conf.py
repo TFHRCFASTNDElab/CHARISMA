3424bdaa0a6e2d722c252a2f831681bbe18ccde9
SteveYangFASTNDE <154364860+SteveYangFASTNDE@users.noreply.github.com>
2024-04-16 09:00:27 -0400

    sphinx documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CHARISMA'
+copyright = '2024, Steve D. Yang, Heng Liu'
+author = 'Steve D. Yang, Heng Liu'
+release = '04/09/2024'
+
+source_suffix = ['.rst']
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_css_files = ['style.css']
